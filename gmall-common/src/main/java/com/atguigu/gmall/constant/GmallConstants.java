cd4abcf8a866345ebb1def9b1638de92302f4f2e
lastingwar <yaohm7788@163.com>
2020-11-05 16:52:18 +0800

    Default Changelist

---
+++ gmall-common/src/main/java/com/atguigu/gmall/constant/GmallConstants.java
@@ -0,0 +1,21 @@
+package com.atguigu.gmall.constant;
+
+/**
+ * @author yhm
+ * @create 2020-11-04 10:49
+ */
+public class GmallConstants {
+
+    public static final String KAFKA_TOPIC_STARTUP="GMALL_STARTUP";
+    public static final String KAFKA_TOPIC_EVENT="GMALL_EVENT";
+
+    public static final String KAFKA_TOPIC_NEW_ORDER="GMALL_NEW_ORDER";
+    public static final String KAFKA_TOPIC_ORDER_DETAIL="GMALL_ORDER_DETAIL";
+
+    public static final String ES_INDEX_DAU="gmall2020_dau";
+    public static final String ES_INDEX_NEW_MID="gmall2020_new_mid";
+    public static final String ES_INDEX_NEW_ORDER="gmall2020_new_order";
+    public static final String ES_INDEX_SALE_DETAIL="gmall2020_sale_detail";
+
+}
+
---
+++ gmall-logger/src/test/java/com/example/gmalllogger/GmallLoggerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.gmalllogger;
+
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallLoggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
