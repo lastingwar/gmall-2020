27c80b8267f00b14129c36374dd7c2f198c67f9b
lastingwar <yaohm7788@163.com>
2020-11-06 19:05:28 +0800

    canal监控mysql发送信息到kafka

--- gmall-common/src/main/java/com/atguigu/gmall/constant/GmallConstants.java
+++ gmall-common/src/main/java/com/atguigu/gmall/constant/GmallConstants.java
@@ -9,6 +9,15 @@ public class GmallConstants {
     public static final String KAFKA_TOPIC_STARTUP="GMALL_STARTUP";
     public static final String KAFKA_TOPIC_EVENT="GMALL_EVENT";
 
+    //启动日志主题
+    public static final String GMALL_STARTUP = "TOPIC_START";
+
+    //事件日志主题
+    public static final String GMALL_EVENT = "TOPIC_EVENT";
+
+    //订单表日志主题
+    public static final String GMALL_ORDER_INFO = "TOPIC_ORDER_INFO";
+
     public static final String KAFKA_TOPIC_NEW_ORDER="GMALL_NEW_ORDER";
     public static final String KAFKA_TOPIC_ORDER_DETAIL="GMALL_ORDER_DETAIL";
 
